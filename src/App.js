4286401805a88c1916af6eab1ce182094118dc6d
Saisarth <119151619+Saisarth@users.noreply.github.com>
2023-11-22 18:02:44 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'; 
 
-function App() {
+const App = () => {
+  const [userData, setUserData] = useState({
+    name: '',
+    gender: '',
+    phone: '',
+    image: '',
+    street:'',
+  });
+
+  useEffect(() => {
+    fetchRandomUser();
+  }, []);
+
+  const fetchRandomUser = () => {
+    fetch('https://randomuser.me/api/?page=1&results=1&seed=abc')
+      .then(response => response.json())
+      .then(data => {
+        const user = data.results[0];
+        setUserData({
+          name: `${user.name.first} ${user.name.last}`,
+          gender: user.gender,
+          phone: user.phone,
+          image: user.picture.large,
+          street:user.street,
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching random user:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app-container">
+      <div className="user-info">
+        <img className="user-image" src={userData.image} alt={`User Image - ${userData.name}`} />
+        <div className="details">
+          <h1>{userData.name}</h1>
+          <p>Gender: {userData.gender}</p>
+          <p>Phone: {userData.phone}</p>
+          
+        
+        
+        <button onClick={fetchRandomUser}>Get Random User</button>
+        </div>
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
+
